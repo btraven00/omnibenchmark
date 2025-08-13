edf2f0d512259ba84c5accdca196d3726b6a2c26
ben <ben.uzh@proton.me>
2025-08-13 14:01:34 +0200

    fix workflow tests

--- tests/workflow/Snakemake_setup.py
+++ tests/workflow/Snakemake_setup.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from omnibenchmark.benchmark import Benchmark
 from omnibenchmark.workflow.snakemake import SnakemakeEngine
 
+# TODO: no need to cleanup anything, just use tmp_dir
 TO_CLEANUP = [".snakemake", "out", "Snakefile", "snakemake.log"]
 
 
@@ -17,11 +18,15 @@ def raise_if_file_not_found(file_path: str):
 
 class SnakemakeSetup:
     def __init__(
-        self, benchmark_file: Path, keep_files: bool = False, cwd: str = ""
+        self,
+        benchmark_file: Path,
+        keep_files: bool = False,
+        cwd: str = "",
+        out_dir: Path = Path("out"),
     ) -> None:
         raise_if_file_not_found(benchmark_file.as_posix())
         self.benchmark_file = benchmark_file
-        self.benchmark = Benchmark(benchmark_file)
+        self.benchmark = Benchmark(benchmark_file, out_dir=out_dir)
         self.workflow = SnakemakeEngine()
         self.keep_files = keep_files
         self.cwd = cwd
@@ -51,15 +56,13 @@ class SnakemakeSetup:
         func(path)
 
     def _print_benchmark(self):
-        benchmark = (
-            self.benchmark.get_converter()
-        )  # This now returns the benchmark directly
+        benchmark = self.benchmark.model
         print(self.benchmark.get_definition())
 
         stages = benchmark.get_stages()
         for stage_id in stages:
             stage = stages[stage_id]
-            stage_name = stage["name"]
+            stage_name = stage.name
             print("Stage", stage_name if stage_name else stage_id)
 
             modules_in_stage = benchmark.get_modules_by_stage(stage)
@@ -78,7 +81,7 @@ class SnakemakeSetup:
 
             for module_id in modules_in_stage:
                 module = modules_in_stage[module_id]
-                module_name = module["name"]
+                module_name = module.name
                 print("  Module", module_name if module_name else module_id)
                 print("    Repo:", benchmark.get_module_repository(module))
                 print("    Excludes:", benchmark.get_module_excludes(module))
